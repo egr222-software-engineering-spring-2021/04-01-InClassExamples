e929e7facc1c30f236aaa973e0ccb99ed5c18d1b
Larry Clement <34249201+lwclement@users.noreply.github.com>
2021-04-01 00:34:44 -0700

    After creating the basic and enhanced simulators

--- DuckSimulator/src/MiniDuckSimulator.java
+++ DuckSimulator/src/MiniDuckSimulator.java
@@ -3,10 +3,5 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
-
-        Duck model = new ModelDuck();
-        model.performFly();
-        model.setFlyBehavior(new FlyRocketPowered());
-        model.performFly();
     }
 }
